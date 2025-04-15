8497ef3bb327ee896818acc1d89fe6f35ace3671
September <202483890031@nuist.edu.cn>
2025-04-15 19:11:04 +0800

    Version1

--- SoilSensor.py
+++ SoilSensor.py
@@ -1,11 +1,9 @@
 import RPi.GPIO as GPIO
-import PCF8591 as ADC
 import time
 
-channel = 21
+channel = 4
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel,GPIO.IN)
-ADC.setup(0*48)
 
 def callback(channel):
         if GPIO.input(channel):
@@ -16,11 +14,5 @@ def callback(channel):
 GPIO.add_event_detect(channel,GPIO.BOTH,bouncetime=300)
 GPIO.add_event_callback(channel,callback)
 
-try:
-    while True:
-        potentiometer_value = ADC.read(0)
-        print(potentiometer_value)
-        time.sleep(0.2)
-
-except KeyboardInterrupt:
-    print("Exit")
+while True:
+        time.sleep(0)
